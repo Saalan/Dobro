97662dd5b8f2df5ca62bf3fba350ac9be6c8c7e8
Saalan <saalan76@gmail.com>
2023-02-17 21:30:51 +0300

    .

--- dobro/urls.py
+++ dobro/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
+ #   path('', include('mainapp.urls')),
 ]
