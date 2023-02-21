aedab7b56c5f89782a60054ecd98ae59c7f11068
Saalan <saalan76@gmail.com>
2023-02-21 22:59:47 +0300

    .

--- bin/arpalert2sql.py
+++ bin/arpalert2sql.py
@@ -1,10 +1,16 @@
 import re
 
 def reader(filename):
+
+    regexp = r'ip=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}),'
+
     with open(filename) as f:
         log = f.read()
+
+        ip_list = re.findall(regexp, log)
     print(log)
+    print(ip_list)
 
 
 if __name__ == "__main__":
-    reader ('arpalert.log')
+    reader('arpalert.log')
