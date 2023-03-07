08fc2c280a2621885a494a286041490c5844f3bd
Saalan <saalan76@gmail.com>
2023-03-07 20:03:46 +0300

    .

--- dobro/settings.py
+++ dobro/settings.py
@@ -77,9 +77,6 @@ WSGI_APPLICATION = 'dobro.wsgi.application'
 
 DATABASES = {
     'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-##      'ENGINE': 'django.db.backends.mysql',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dobro',
         'USER' : 'dobro_dbuser',
@@ -88,7 +85,6 @@ DATABASES = {
         'PORT' : '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from netfields import CidrAddressField, MACAddressField, NetManager
 
+# pip install django-netfields
 # Create your models here.
+
+class arp(models.Model):
+    class Meta:
+        db_table = 'arpalert_log'
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField(null=True)
+    mac = MACAddressField(null=True)
+    ipv4 = CidrAddressField(null=True)
+    vendor = models.CharField(null=True, max_length=100)
+
+    objects = NetManager()
