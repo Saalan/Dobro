e4ad7adeb6395cce6509362f93852a875daec89c
Alexander Sadkov <saalan76@gmail.com>
2023-03-08 23:45:15 +0300

    .

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,7 +9,7 @@ class arpalert_log(models.Model):
         db_table = 'arpalert_log'
         verbose_name = 'ARP лог'
         verbose_name_plural = 'ARP логи'
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, verbose_name ='ID')
     time = models.DateTimeField(null=True)
     mac = MACAddressField(null=True)
     ipv4 = CidrAddressField(null=True)
@@ -19,3 +19,22 @@ class arpalert_log(models.Model):
 
     def __str__(self):
         return self.vendor
+
+#class dhcp_conf(models.Model):
+#    class Meta:
+#        db_table = 'net_device'
+#        verbose_name = 'DHCP'
+#        verbose_name_plural = 'DHCP'
+#    id = models.AutoField(primary_key=True, verbose_name ='ID')
+#    cfg_id = models.IntegerField()
+#    group = models.SmallIntegerField(default='0')
+#    mac = MACAddressField()
+#    ipv4 = CidrAddressField()
+#    host = models.CharField(max_length=63)
+#    comments = models.TextField(max_length=1024)
+#    time = models.DateTimeField(auto_now_add=True, blank=True)
+#
+#    objects = NetManager()
+
+#    def __str__(self):
+#        return self.host
