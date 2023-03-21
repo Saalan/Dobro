0ef83b60cf83b3dc1b81b28c75747bbb851dbb26
Saalan <saalan@gmail.com>
2023-03-21 19:21:25 +0300

    .

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import arpalert_log
+from .models import arpalert_log, net_device
 
 # Register your models here.
 
 admin.site.register(arpalert_log)
+admin.site.register(net_device)
--- mainapp/models.py
+++ mainapp/models.py
@@ -9,7 +9,7 @@ class arpalert_log(models.Model):
         db_table = 'arpalert_log'
         verbose_name = 'ARP лог'
         verbose_name_plural = 'ARP логи'
-    id = models.AutoField(primary_key=True, verbose_name ='ID')
+    id = models.AutoField(primary_key=True, verbose_name='ID')
     time = models.DateTimeField(null=True)
     mac = MACAddressField(null=True)
     ipv4 = CidrAddressField(null=True)
@@ -20,21 +20,21 @@ class arpalert_log(models.Model):
     def __str__(self):
         return self.vendor
 
-#class dhcp_conf(models.Model):
-#    class Meta:
-#        db_table = 'net_device'
-#        verbose_name = 'DHCP'
-#        verbose_name_plural = 'DHCP'
-#    id = models.AutoField(primary_key=True, verbose_name ='ID')
-#    cfg_id = models.IntegerField()
-#    group = models.SmallIntegerField(default='0')
-#    mac = MACAddressField()
-#    ipv4 = CidrAddressField()
-#    host = models.CharField(max_length=63)
-#    comments = models.TextField(max_length=1024)
-#    time = models.DateTimeField(auto_now_add=True, blank=True)
-#
-#    objects = NetManager()
+class net_device(models.Model):
+    class Meta:
+        db_table = 'net_device'
+        verbose_name = 'DHCP conf'
+        verbose_name_plural = 'DHCP conf'
+    id = models.AutoField(primary_key=True, verbose_name='ID')
+    cfg_id = models.IntegerField()
+    group = models.SmallIntegerField(default='0')
+    mac = MACAddressField()
+    ipv4 = CidrAddressField()
+    host = models.CharField(max_length=63)
+    comments = models.TextField(max_length=1024)
+    time = models.DateTimeField(auto_now_add=True, blank=True)
 
-#    def __str__(self):
-#        return self.host
+    objects = NetManager()
+
+    def __str__(self):
+        return self.host
